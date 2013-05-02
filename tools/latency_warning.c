241634cef19118171e26dd25f7f7845f4e3e78c1
Coly Li <i@coly.li>
2013-05-02 19:59:32 +0800

    dm-latency: demo program to use lib_dm_io_latency
    Add a demo program latency_warning.c to show how to use lib_dm_io_latency library.

---
+++ tools/latency_warning.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int is_dm_target_io_latency_ok(const char *target_name,
+				int latency_threshold,
+				int latency_warning_nr);
+
+int main()
+{
+	int ret;
+	char *dev = "dm-0";
+
+	ret = is_dm_target_io_latency_ok(dev, 0, 0);
+	printf("latency of %s is ", dev);
+
+	if (ret == 0)
+		printf("bad");
+	else if (ret == 1)
+		printf("ok");
+	else
+		printf("error");
+
+	printf("\n");
+
+	return 0;
+}
