6cec1cd192b15181970a5b66e6fc1d18320dc261
Coly Li <i@coly.li>
2013-05-27 15:39:10 +0800

    dm-latency: add script to setup dm target
    target-setup.sh MUST be executed after modules-setup.sh get run.
    This scripts just sets up runtime linear dm target, after this, system
    administrators are able to mount existing file systems on the linear dm
    targets.

---
+++ scripts/target-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+set e
+
+# fdisk lists device size in 1K blocks, get_target_size()
+# return device size in 512bytes sectors.
+get_target_size() {
+	dev=$1
+	block_size=`fdisk -l | grep "$dev" | sed "s/\*//g" | awk '{print $4}'`
+	expr "$block_size" \* 2
+}
+
+for dev in b1 c1 d1 e1 f1 g1 h1 i1 j1 k1 l1;do
+	size=`get_target_size sd$dev`
+	echo "0 $size linear /dev/sd$dev 0" | dmsetup create sd"$dev"
+done
